3739b9963308cd24a20e948fac770b20b81497c8
timrozer <timrozer@gmail.com>
2023-09-14 17:19:09 -0400

    changed resume

--- js/script.js
+++ js/script.js
@@ -45,6 +45,25 @@ window.onscroll = () => {
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
 
+  function timeline() {
+  var reveals = document.querySelectorAll(".timeline");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", timeline);
+
+// To check the scroll position on page load
+timeline();
+
 // animation footer on scroll
 // let footer = document.querySelector('footer');
 //
